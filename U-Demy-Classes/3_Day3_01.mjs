be23e04b623172e5971a03528e083d6ccf7880c6
Nazeem Khan <nazeemkhanpk@gmail.com>
2023-10-29 03:32:16 +0500

    initial commit

---
+++ U-Demy-Classes/3_Day3_01.mjs
@@ -0,0 +1,9 @@
+// Activating Strict Moder in java script.
+'use strict';
+// How stict mode works
+
+let hasDriversLicense = false;
+const passTest= true;
+if (passTest) hasDriverLicense=true; // In this line i forgest s in hasDriver .
+// if i run it without strict mode it didn't show me the error in console.
+if (hasDriversLicense) console.log('I can Drive')
